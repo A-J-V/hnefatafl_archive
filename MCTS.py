3902a291a9ffe755156e7547d92aaa7ac67abc71
AJV <alexander.valarus@gmail.com>
2023-10-29 11:09:04 -0400

    Refactored functions that operate on nodes to be Node class methods.

--- MCTS.py
+++ MCTS.py
@@ -13,39 +13,35 @@ class Node:
         self.visits = 0
         self.value = 0
 
+    def best_child(self):
+        return max(self.children, key=lambda x: x.value / (x.visits if x.visits != 0 else 1))
+
+    def ucb1(self, c):
+        """Calculate the UCB1 value using exploration factor c."""
+        return ((self.value / (self.visits if self.visits != 0 else 1)) +
+                c * (np.log(self.parent.visits) / (self.visits if self.visits != 0 else 1)) ** 0.5
+                )
+
+    def select_node(self):
+        """Use the UCB1 formula to select a node"""
+        best = max(self.children, key=self.ucb1)
+        return best if best else None
+
+    def expand_node(self):
+        actions = all_legal_moves(self.state, self.player)
+        for row in actions.shape[1]:
+            for col in actions.shape[2]:
+                for action in actions[:, row, col]:
+                    if action == 0:
+                        continue
+                    new_state = np.array(self.state)
+                    make_move(new_state, (row, col), action)
+                    new_node = Node(new_state, player=toggle_player(self.player))
+                    self.children.append(new_node)
+
 
 def toggle_player(player):
     return "defenders" if player == "attackers" else "attackers"
-
-
-def best_child(node):
-    return max(node.children, key=lambda x: x.value / (x.visits if x.visits != 0 else 1))
-
-
-def ucb1(node, c):
-    """Calculate the UCB1 value using exploration factor c."""
-    return ((node.value / (node.visits if node.visits != 0 else 1)) +
-            c * (np.log(node.parent.visits) / (node.visits if node.visits != 0 else 1)) ** 0.5
-            )
-
-
-def select_node(node):
-    """Use the UCB1 formula to select a node"""
-    best = max(node.children, key=ucb1)
-    return best if best else None
-
-
-def expand_node(node):
-    actions = all_legal_moves(node.state, node.player)
-    for row in actions.shape[1]:
-        for col in actions.shape[2]:
-            for action in actions[:, row, col]:
-                if action == 0:
-                    continue
-                new_state = np.array(node.state)
-                make_move(new_state, (row, col), action)
-                new_node = Node(new_state, player=toggle_player(node.player))
-                node.children.append(new_node)
 
 
 def simulate(board, player, visualize=False):
