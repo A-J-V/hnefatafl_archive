cfa51b703b030b2e252739ca4fcf7f1113d907be
AJV <alexander.valarus@gmail.com>
2023-12-21 14:32:38 -0600

    Updated model for the bootstrapped CNN

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from torch import nn
 
 class ResNet_initial_block(nn.Module):
     """
-    Accept (batch_size, 3, 11, 11) shaped tensor and pass it through
+    Accept (batch_size, 3, 224, 224) shaped tensor and pass it through
     a 7x7 convolution and MaxPool layer.
     """
 
@@ -14,17 +14,24 @@ class ResNet_initial_block(nn.Module):
         # The convolution, batchnorm, and ReLU activation.
         # Remember to add the padding!
         self.resnet_block1 = nn.Sequential(nn.Conv2d(in_channels=3,
-                                                     out_channels=32,
+                                                     out_channels=64,
                                                      kernel_size=(7, 7),
                                                      stride=1,
                                                      padding='same'
                                                      ),
-                                           nn.BatchNorm2d(32),
+                                           nn.BatchNorm2d(64),
                                            nn.ReLU(),
                                            )
 
+        # The Max Pool layer. Remember to add the padding!
+        # self.pool = nn.MaxPool2d(kernel_size=3,
+        #                         stride=2,
+        #                         padding=1,
+        #                        )
+
     def forward(self, x):
         x = self.resnet_block1(x)
+        # x = self.pool(x)
         return x
 
 
@@ -103,24 +110,33 @@ class ResNet_transition_block(nn.Module):
         return self.relu(x)
 
 
-class BabyViking(nn.Module):
-    """A miniature ResNet."""
+class ResNet(nn.Module):
+    """Model architecture based on the the paper 'Deep Residual Learning for Image Recognition'
+       by He, Zhang, Ren, and Sun (2015).
+
+       This is the 34-layer version.
+       """
 
     def __init__(self, classes=1) -> None:
         super().__init__()
 
-        self.resnetmicro = nn.Sequential(*[
+        self.resnet = nn.Sequential(*[
             ResNet_initial_block(),
-            ResNet_transition_block(32, 64),
+            nn.Sequential(*(ResNet_basic_block(64) for _ in range(3))),
             ResNet_transition_block(64, 128),
+            nn.Sequential(*(ResNet_basic_block(128) for _ in range(3))),
             ResNet_transition_block(128, 256),
+            nn.Sequential(*(ResNet_basic_block(256) for _ in range(4))),
+            ResNet_transition_block(256, 512),
+            nn.Sequential(*(ResNet_basic_block(512) for _ in range(2))),
+            # nn.AvgPool2d(kernel_size=(7, 7)),
             nn.Flatten(),
-            nn.Linear(in_features=1024, out_features=classes),
-            nn.Sigmoid()
+            nn.Linear(in_features=2048, out_features=classes),
+            nn.Sigmoid(),
         ])
 
     def forward(self, x):
-        return self.resnetmicro(x)
+        return self.resnet(x)
 
 
 def load_baby_viking():
